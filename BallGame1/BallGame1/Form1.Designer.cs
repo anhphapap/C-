a2092d94df4293d86d9ad97f2b08422bda369dd1
Anh Pha <anhphapap0@gmail.com>
2024-05-15 16:24:43 +0700

    Fix BallGame

---
+++ BallGame1/BallGame1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace BallGame1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeBackgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = global::BallGame1.Properties.Resources.pngwing_com;
+            this.ball.Location = new System.Drawing.Point(348, 149);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(80, 80);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeImageToolStripMenuItem,
+            this.changeBackgroundColorToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // changeImageToolStripMenuItem
+            // 
+            this.changeImageToolStripMenuItem.Name = "changeImageToolStripMenuItem";
+            this.changeImageToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.changeImageToolStripMenuItem.Text = "Change Image";
+            this.changeImageToolStripMenuItem.Click += new System.EventHandler(this.changeImageToolStripMenuItem_Click);
+            // 
+            // changeBackgroundColorToolStripMenuItem
+            // 
+            this.changeBackgroundColorToolStripMenuItem.Name = "changeBackgroundColorToolStripMenuItem";
+            this.changeBackgroundColorToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.changeBackgroundColorToolStripMenuItem.Text = "Change Background Color";
+            this.changeBackgroundColorToolStripMenuItem.Click += new System.EventHandler(this.changeBackgroundColorToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(211, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeBackgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BallGame1/BallGame1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallGame1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int move = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (move)
+            {
+                case 0:
+                    {
+                        ball.Left -= 10;
+                        if (ball.Left < -80)
+                            ball.Left = ClientRectangle.Width;
+                    }
+                    break;
+                case 1:
+                    {
+                        ball.Top -= 10;
+                        if (ball.Top < -80)
+                            ball.Top = ClientRectangle.Height;
+                    }
+                    break;
+                case 2:
+                    {
+                        ball.Left += 10;
+                        if (ball.Left >= ClientRectangle.Width)
+                            ball.Left = -80;
+                    }
+                    break;
+                case 3:
+                    {
+                        ball.Top += 10;
+                        if (ball.Top >= ClientRectangle.Height)
+                            ball.Top = -80;
+                    }
+                    break;
+            }
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.S:
+                    {
+                        if (!timer1.Enabled) 
+                            timer1.Enabled = true; 
+                        else
+                            timer1.Enabled = false;
+                    }
+                    break;
+                case Keys.Up: move = 1; break;
+                case Keys.Down: move = 3; break;
+                case Keys.Left: move = 0; break;
+                case Keys.Right: move = 2; break;
+                case Keys.U: timer1.Interval -= (timer1.Interval > 10) ? 10 : 0; break;
+                case Keys.D: timer1.Interval += (timer1.Interval < 200) ? 10 : 0; break;
+            }
+            return true;
+        }
+
+        private void changeImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image (*.jpg)|*.jpg";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.BackgroundImage = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void changeBackgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.FullOpen = true;
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.BackgroundImage = null;
+                this.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Đóng ứng dụng?","Cảnh báo",MessageBoxButtons.OKCancel,MessageBoxIcon.Warning);
+        }
+    }
+}
